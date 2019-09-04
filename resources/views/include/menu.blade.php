a72d4471f65b0dc646357609aa7aa955bbdfce29
George <jorgenietom83@gmail.com>
2019-09-03 23:47:40 -0500

    menu

---
+++ resources/views/include/menu.blade.php
@@ -0,0 +1,88 @@
+<div class="sp-body">
+
+    <!-- Loader Landing Page -->
+    <div id="loader">
+        <div class="loader"></div>
+    </div>
+
+    <!--<header id="header">
+        <div class="header-top">
+        </div>
+
+        <div class="header-middle">
+            <div class="container relative">
+                <a href="index.html" class="logo pull-left">hgkjhgkjhgjhg</a>
+                <div class="header-search pull-right">
+
+
+                </div>
+            </div>
+        </div>
+    </header>-->
+
+    <!-- icon-menu -->
+    <nav id="icon-menu" class="main-menu">
+
+        <div class="scrollbar" id="style-1">
+            <ul>
+                <li class="lightli">
+                    <a href="">
+                        <i class="fa fa-users fa-lg text-danger"></i>
+                        <span class="nav-text">CONTROL ESCOLAR</span>
+                    </a>
+                </li>
+                <li class="lightli">
+                    <a href="">
+                        <!--<i class="fa fa-database fa-lg text-danger"></i>-->
+                        <i class="fa fa-server fa-lg text-danger"></i>
+                        <span class="nav-text">BASE DE DATOS</span>
+                    </a>
+                </li>
+                <li class="lightli">
+                    <a href="">
+                        <i class="fa fa-check-square fa-lg text-danger"></i>
+                        <span class="nav-text">CALIFICACIONES</span>
+                    </a>
+                </li>
+                <li class="lightli">
+                    <a href="">
+                        <i class="fa fa-money-bill-alt fa-lg text-danger"></i>
+                        <span class="nav-text">COLEGIATURAS</span>
+                    </a>
+                </li>
+
+                <li class="lightli">
+                    <a href="">
+                        <i class="fa fa-level-down-alt fa-lg text-danger"></i>
+                        <span class="nav-text">BAJAS</span>
+                    </a>
+                </li>
+
+                <!--<li class="lightli">
+                    <a href="">
+                        <i class="fa fa-map-marked fa-lg text-danger"></i>
+                        <span class="nav-text">RUTAS</span>
+                    </a>
+                </li>
+
+                <li class="lightli">
+                    <a href="">
+                        <i class="fa fa-file-alt fa-lg text-danger"></i>
+                        <span class="nav-text">Carta Porte</span>
+                    </a>
+                </li>
+
+                <li class="lightli">
+                    <a href="">
+                        <i class="fa fa-file-alt fa-lg text-danger"></i>
+                        <span class="nav-text">Cuentas Por Cobrar</span>
+                    </a>
+                </li>-->
+
+            </ul>
+        </div>
+
+    </nav>
+
+
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,15 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="{{url('controlEscolar')}}"><img src="{{asset('img/englishinlive.jpeg')}}" alt="" width="400px"></a>
                 </div>
             </div>
         </div>
